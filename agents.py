84feaa1076d8c8127aadf2a2c94c9e3a1fb46d81
Akshay Mohabey <akshay.mohabey@gmail.com>
2024-07-11 17:56:26 +0530

    Error Finding

--- agents.py
+++ agents.py
@@ -13,27 +13,27 @@ import mesa
 
 # Citizen Class
 class Citizen(mesa.Agent):
-    def __init__(self,citizen_id, model):
+    def __init__(self,citizen_id,model):
         super().__init__(citizen_id, model)
         print("Hello, I am Citizen:",citizen_id)
 
-    def step():
-        pass
+    # def step():
+    #     pass
 
 # Militant Class 
 class Militant(mesa.Agent):
-    def __init__(self, militant_id, model):
+    def __init__(self,militant_id,model):
         super().__init__(militant_id,model)
         print("Hello, I am Militant:",militant_id)
 
-    def step():
-        pass
+    # def step():
+        # pass
 
 # Military Squad
 class MilitarySquad(mesa.Agent):
-    def __int__(self, military_id, model):
+    def __int__(self,military_id,model):
         super().__init__(military_id, model)
         print("Hello, We are Military Squad:", military_id)
 
-    def step():
-        pass
+    # def step():
+    #     pass
--- model.py
+++ model.py
@@ -14,27 +14,30 @@ import agents
 
 class Model(mesa.Model):
 
-    def __init__(self, citizens, militants, military_squad):
+    def __init__(self, citizens,militants,military_squad):
         super().__init__()
         self.num_of_citizens = citizens
+        self.num_of_militants = militants
+        self.num_of_msquad = military_squad
+
         self.citizens_list = []
         self.militants_list = []
         self.military_squads_list = []
 
-        # Creating Military Squad
-        for k in range(military_squad):
-            z = agents.MilitarySquad(k,self)
-            self.military_squads_list.append(z)
-
         # Creating Citizens
-        for i in range(citizens):
+        for i in range(self.num_of_citizens):
             x = agents.Citizen(i,self)
             self.citizens_list.append(x)
         
         # Creating Militants
-        for j in range(militants):
+        for j in range(self.num_of_militants):
             y = agents.Militant(j,self)
             self.militants_list.append(y)
+        
+        # Creating Military Squad
+        for k in range(5):
+            z = agents.MilitarySquad(k,self)
+            self.military_squads_list.append(z)
 
     # Setter and getter functions
     def get_citizens_list(self):
